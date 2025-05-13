eafc8b95b65981cf29e7f811c6e631b5e6e25c29
tanuja23370 <tanujanimbakkanavar23@gmail.com>
2025-05-13 09:53:59 -0300

    save

---
+++ StudentCourses.java
@@ -0,0 +1,43 @@
+package Prgm4StudCourse;
+
+import java.util.*;
+
+public class StudentCourses {
+    private Map<Integer, List<Course>> courseMap;
+
+    public StudentCourses() {
+        courseMap = new HashMap<>();
+    }
+
+    public void addCourse(int semester, String courseName, int marks) {
+        courseMap.computeIfAbsent(semester, k -> new ArrayList<>())
+                 .add(new Course(courseName, marks));
+    }
+
+    public void displayCourses() {
+        for (int semester : courseMap.keySet()) {
+            System.out.println("Semester " + semester + ":");
+            for (Course c : courseMap.get(semester)) {
+                System.out.println("  " + c.courseName + " - Marks: " + c.marks);
+            }
+        }
+    }
+
+    private static class Course {
+        String courseName;
+        int marks;
+
+        Course(String courseName, int marks) {
+            this.courseName = courseName;
+            this.marks = marks;
+        }
+    }
+
+    public static void main(String[] args) {
+        StudentCourses sc = new StudentCourses();
+        sc.addCourse(1, "Math", 80);
+        sc.addCourse(1, "English", 75);
+        sc.addCourse(2, "Science", 90);
+        sc.displayCourses();
+    }
+}
